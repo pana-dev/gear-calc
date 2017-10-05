02386c0045fe971cd227a6063721b11144ab0729
Pana <c9.pana@gmail.com>
2017-10-04 23:12:38 -0400

    Cleaned up code

--- app.js
+++ app.js
@@ -1,154 +1,151 @@
-const config = {
-    API_KEY: "8k7kh3mvnanft46vcqv93ezgagcm2qsw",
-    FIELDS: ["items+", "professions+", "stats+", "talents"]
-};
+//////////////////////////////////////
+// Set API Key and Fields to return
+//////////////////////////////////////
+const API_KEY = "8k7kh3mvnanft46vcqv93ezgagcm2qsw";
 
-function configureUrl(realmInput, characterInput, regionInput) {
+// Function that builds the API URL
+//////////////////////////////////////
 
-    if (regionInput == 'en_us') {
-        var base_url = 'us';
+function characterAPI(name, realm, region, field) {
+
+    let base_url = null;
+
+    if (region == 'en_us') {
+        base_url = 'us';
     }
 
-    if (regionInput == 'en_GB') {
-        var base_url = 'eu';
+    if (region == 'en_GB') {
+        base_url = 'eu';
     }
 
-    if (regionInput == 'ko_KR') {
-        var base_url = 'kr';
+    if (region == 'ko_KR') {
+        base_url = 'kr';
     }
 
-    if (regionInput == 'zh_TW') {
-        var base_url = 'tw'
+    if (region == 'zh_TW') {
+        base_url = 'tw'
     }
 
-    return `https://${base_url}.api.battle.net/wow/character/${realmInput.value}/${characterInput.value}?fields=${config.FIELDS}&locale=${regionInput}&apikey=${config.API_KEY}`;
+    return `https://${base_url}.api.battle.net/wow/character/${realm}/${name}?fields=${field}&locale=${region}&apikey=${API_KEY}`;
+
 }
 
-function createItemRow(item) {
-    // Create different elements for each row
-    const row = document.createElement("div"),
-        media = document.createElement("div"),
-        imageWrap = document.createElement("div"),
-        infoWrap = document.createElement("div"),
-        name = document.createElement("div"),
-        level = document.createElement("div");
+// Function to create an item
+//////////////////////////////////////
 
-    if (item.icon) {
-        const image = document.createElement("img");
+function createRowItem(item) {
 
-        // Set src and alt text for the image
-        image.setAttribute(
-            "src",
-            `http://media.blizzard.com/wow/icons/36/${item.icon}.jpg`
-        );
+    // 1. Set Data
 
-        // image.setAttribute("alt", item.name);
+        const itemData = {
+            IMAGE: item.icon,
+            NAME: item.name,
+            LEVEL: item.itemLevel
+        };
 
-        // Insert image into image wrap
-        imageWrap.appendChild(image);
-    }
+    // 2. Create content elements
 
-    // Add class to row (li) element
-    row.classList.add("panel-block");
+        const itemContent = {
+            NAME: document.createElement('div'),
+            LEVEL: document.createElement('div'),
+            IMAGE: document.createElement('img')
+        };
 
-    // Add class to media object
-    media.classList.add('media');
+    // 3. Create structural elements
 
-    // Add class to image wrap
-    imageWrap.classList.add("media-left");
+        const rowStructure = {
+            WRAP: document.createElement("div"),
+            MEDIA: document.createElement("div"),
+            MEDIA_LEFT: document.createElement("div"),
+            MEDIA_CONTENT: document.createElement("div")
+        };
 
-    // Add class to item information wrap
-    infoWrap.classList.add("media-content");
-    infoWrap.classList.add('is-size-7');
+    // 4. Add styling to elements
 
-    // Set text for the item name and level
-    name.textContent = item.name;
-    level.textContent = item.itemLevel;
+        rowStructure.WRAP.classList.add("panel-block");
+        rowStructure.MEDIA.classList.add('media');
+        rowStructure.MEDIA_LEFT.classList.add("media-left");
+        rowStructure.MEDIA_CONTENT.classList.add("media-content");
+        rowStructure.MEDIA_CONTENT.classList.add('is-size-7');
 
-    // Insert name and level within infowrap
-    infoWrap.appendChild(name);
-    infoWrap.appendChild(level);
+    // 5. Construct the Front-End
 
-    // Insert image and info into each row
+        rowStructure.WRAP.appendChild(rowStructure.MEDIA);
+            rowStructure.MEDIA.appendChild(rowStructure.MEDIA_LEFT);
+                if (item.icon) {
 
-    media.appendChild(imageWrap);
-    media.appendChild(infoWrap);
-    row.appendChild(media);
+                    // Set src and alt text for the image
+                    itemContent.IMAGE.setAttribute(
+                        "src",
+                        `http://media.blizzard.com/wow/icons/36/${itemData.IMAGE}.jpg`
+                    );
 
-    // Output row`
-    return row;
+                    // image.setAttribute("alt", item.name);
+                    rowStructure.MEDIA_LEFT.appendChild(itemContent.IMAGE);
+                }
+            rowStructure.MEDIA.appendChild(rowStructure.MEDIA_CONTENT);
+                rowStructure.MEDIA_CONTENT.appendChild(itemContent.NAME);
+                    itemContent.NAME.textContent = itemData.NAME;
+                rowStructure.MEDIA_CONTENT.appendChild(itemContent.LEVEL);
+                    itemContent.LEVEL.textContent = itemData.LEVEL;
+
+    // 6. Render Row
+        return rowStructure.WRAP;
+
 }
+
+// Fetch data and create items
+//////////////////////////////////////
 
 class characterItems {
+
     constructor(element) {
         this.element = element;
-        this.form = this.element.querySelector(".search-character");
-        this.button = this.element.querySelector(".app-button");
-        this.characterInput = this.element.querySelector("#character");
-        this.realmInput = this.element.querySelector("#realm");
-        this.regionInput = this.element.querySelector("#region");
-        this.dataWrap = this.element.querySelector(".app-data");
+        this.formCharacter = this.element.querySelector("#formCharacter");
+        this.characterName = this.element.querySelector("#characterName");
+        this.characterRealm = this.element.querySelector("#characterRealm");
+        this.characterRegion = this.element.querySelector("#characterRegion");
+        this.wrap = this.element.querySelector("#itemsArmory");
         this.init();
     }
 
-    renderItems(items) {
-        const itemsTitle = document.createElement('p'),
-            panelBlock = document.createElement('div'),
-            panelControl = document.createElement('p'),
-            panelInput = document.createElement('input');
+    renderItems(data) {
 
-        itemsTitle.innerHTML = 'Items';
-        itemsTitle.classList.add('panel-heading');
+        for (let key in data.items) {
 
-        panelBlock.classList.add('panel-block');
-        panelControl.classList.add('control');
-        panelInput.classList.add('input');
-        panelInput.classList.add('is-small');
-        panelInput.setAttribute('placeholder', 'Paste Pawn string');
-
-        this.dataWrap.appendChild(itemsTitle);
-        panelControl.appendChild(panelInput);
-        panelBlock.appendChild(panelControl);
-        this.dataWrap.appendChild(panelBlock);
-
-        for (let key in items) {
-            const item = items[key];
+            const item = data.items[key];
 
             if (!item.icon) {
                 continue;
             }
 
-            const itemRow = createItemRow(item);
-            this.dataWrap.appendChild(itemRow);
+            const newRow = createRowItem(item);
+            this.wrap.appendChild(newRow);
         }
-    }
 
-    renderData(characterData) {
-        const { items, stats, talents } = characterData;
-
-        if (items) {
-            this.renderItems(items);
-        }
     }
 
     fetchData(event) {
-        event.preventDefault();
-        // Cheap clear on new fetch
-        this.dataWrap.innerHTML = null;
-        // Cheap loading flag
-        this.dataWrap.classList.add('is-loading');
 
-        fetch(configureUrl(this.realmInput, this.characterInput, this.regionInput.value))
+        event.preventDefault();
+        this.wrap.innerHTML = null;
+        this.wrap.classList.add('is-loading');
+
+        fetch(characterAPI(this.characterName.value, this.characterRealm.value, this.characterRegion.value, 'items'))
             .then(response => response.json())
-            .then((characterData) => {
-                this.dataWrap.classList.remove('is-loading');
-                this.renderData(characterData);
+            .then((data) => {
+                this.renderItems(data);
+            })
+            .catch((error) => {
+                console.log('Could not find character' + error.message);
             });
+
     }
 
     init() {
-        this.form.addEventListener("submit", this.fetchData.bind(this));
+        this.formCharacter.addEventListener("submit", this.fetchData.bind(this));
     }
+    
 }
 
 const app = document.querySelector(".app");
